67048d201feb072d908d97ac66359ab00f7aca7f
Seanna Musgrave <seannamusgrave@gmail.com>
2014-04-21 22:06:32 -0700

    Added continent question

--- game.rb
+++ game.rb
@@ -4,6 +4,7 @@ class StoryBranch
 	attr_accessor :prompt
 	attr_accessor :options
 	attr_accessor :decision
+	attr_accessor :textEntry
 
 	def process memory
 		return
@@ -23,18 +24,31 @@ event3 = StoryBranch.new()
 
 def event3.process memory
 
-    print memory
-
     if memory["gender"] == "1"
-    	@text = "Booga!"
-    else
-    	@text = "Nooga."
+    	@text = "Boys are interesting."
+    elsif memory["gender"] == "2"
+    	@text = "Girls are pretty."
+    elsif memory["gender"] == "3"
+    	@text = "Iiiiiinnteresting"
     end
 end
 
+event4 = StoryBranch.new()
+event4.text = "What is your name?"
+event4.decision = "name"
+event4.prompt = true
+event4.textEntry = true
+
+event5 = StoryBranch.new()
+event5.text = "What continent are you from?"
+event5.prompt = true
+event5.options = {"1" => "Europe", "2" => "Asia", "3" => "Africa"}
+event5.decision = "continent"
 
 events.next = event2
 event2.next = event3
+event3.next = event4
+event4.next = event5
 
 # Roll this off into its own function.
 
@@ -51,19 +65,20 @@ def gameloop events
 		print currentEvent.text
 
 		if (currentEvent.prompt)
-	        print "\n Your options are:"
-	        currentEvent.options.each do |hash, key|
-	        	print "\n"
-	        	print hash
-	        	print " : "
-	        	print key
-	        end
-	        print "\n"
+			print "\n"
+			if (!currentEvent.textEntry) 
+		        print "Your options are:"
+		        currentEvent.options.each do |hash, key|
+		        	print "\n"
+		        	print hash
+		        	print " : "
+		        	print key
+		        end
+		        print "\n"
+		    end
 	        response = gets.chomp()
 
 	        storyMemory[currentEvent.decision] = response
-
-
 		end
 
 		if currentEvent.next == nil
